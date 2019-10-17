4b75562831721f193f47c9a6668d0aef499b9c69
Paulo Oliveira <paulo.oliveira@corp.globo.com>
2019-10-17 13:35:29 -0300

    feat(home): Add jinkajs

--- src/App.js
+++ src/App.js
@@ -1,9 +1,21 @@
 import React from "react";
-import ReactDOM from "react-dom";
+import jikanjs from "jikanjs";
 
 class App extends React.Component {
     render() {
-        return <h1> Hello World :) </h1>;  }}
+        jikanjs
+        .loadSeasonLater()
+        .then((response) => {
+            response.anime.forEach(element => {
+                console.log(`${element.mal_id}: ${element.title} - ${element.image_url} - ${element.type}`);
+            })
+        }).catch((err) => {
+            console.error(err); // in case a error happens
+        });
+
+        return <h1> Hello World :) </h1>;
+    }
+}
 
 
 export default App;
