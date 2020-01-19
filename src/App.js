c4f3ede6fcfe8c0a631ee4c636577ec50b6563a1
Paulo Oliveira <paulofrauches@gmail.com>
2020-01-19 17:48:41 -0300

    feat(App): Add simple Navigation

--- src/App.js
+++ src/App.js
@@ -9,31 +9,95 @@ class App extends React.Component {
     super(props)
     this.state = {
       animes: [],
+      activeItem: 0,
     }
+    this.onKeyDown = this.onKeyDown.bind(this)
   }
 
   componentDidMount() {
     const animes = []
     jikanjs
       .loadSeasonLater()
-      .then((response) => {
-        response.anime.forEach((element) => {
+      .then(response => {
+        response.anime.forEach(element => {
           animes.push(element)
         })
         this.setState({ animes })
       })
-      .catch((err) => {
+      .catch(err => {
         window.console.error(err) // in case a error happens
       })
+
+    this.enableKeyEvent()
   }
 
-  tmpl(anime) {
-    const {
-      image_url, title, mal_id
-    } = anime
+  componentWillUnmount() {
+    this.disableKeyEvent()
+  }
+
+  enableKeyEvent() {
+    document.addEventListener('keydown', this.onKeyDown, true)
+  }
+
+  disableKeyEvent() {
+    document.removeEventListener('keydown', this.onKeyDown, true)
+  }
+
+  onKeyDown(e) {
+    const { activeItem, animes } = this.state
+    console.log('->', animes.length)
+    let newActiveItem = activeItem
+    switch (e.keyCode) {
+      case 37: // left
+        if (activeItem !== 0) {
+          newActiveItem -= 1
+        }
+        break
+
+      case 39: // right
+        if (activeItem < animes.length) {
+          newActiveItem += 1
+        }
+        break
+
+      case 40: // down
+        if (activeItem < animes.length) {
+          if (activeItem + 4 > animes.length) {
+            newActiveItem = animes.length
+          } else {
+            newActiveItem += 4
+          }
+        }
+        break
+      case 38: // up
+        if (activeItem < animes.length) {
+          if (activeItem - 4 < 0) {
+            newActiveItem = 0
+          } else {
+            newActiveItem -= 4
+          }
+        }
+        break
+      default:
+        break
+    }
+    this.setState({
+      activeItem: newActiveItem
+    })
+  }
+
+  tmpl(anime, i) {
+    const { image_url, title, mal_id } = anime
+    const { activeItem } = this.state
 
     return (
-      <div className="poster-wrapper" id={mal_id} key={mal_id}>
+      <div
+        className={`poster-wrapper ${
+          activeItem === i ? 'poster-wrapper--active' : null
+        }`}
+        id={mal_id}
+        key={mal_id}
+      >
         <figure>
           <img src={image_url} alt={title} />
           <p>{title}</p>
@@ -44,7 +108,10 @@ class App extends React.Component {
 
   render() {
     const { animes } = this.state
-    return animes.map((anime) => anime.type == 'TV' ? this.tmpl(anime) : null)
+    console.log( this.state.activeItem )
+    return animes.map((anime, i) =>
+      anime.type === 'TV' ? this.tmpl(anime, i) : null
+    )
   }
 }
 
--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,16 @@
 import React from 'react';
-import { shallow } from 'enzyme';
+import { shallow, mount, render } from 'enzyme';
 import App from './App';
 
-it('renders without crashing', () => {
-  const component = shallow(<App />);
-  expect(component.find('div')).toHaveLength(1);
-});
+describe('App', () => {
+  it('Should match snapshot', () => {
+    const component = shallow(<App />);
+    expect(component).toMatchSnapshot()
+  });
+
+  it('renders without crashing', () => {
+    const component = mount(<App />);
+    expect(component).toHaveLength(1);
+  });
+})
+
