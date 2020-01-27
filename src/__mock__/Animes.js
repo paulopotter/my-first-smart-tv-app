f572f5c27c5bd9ed2b1625f971aa8e6a84021292
Paulo Oliveira <paulofrauches@gmail.com>
2020-01-27 06:39:17 -0300

    test(mock): Add mock

---
+++ src/__mock__/Animes.js
@@ -0,0 +1,546 @@
+/* eslint-disable camelcase */
+const Animes = [
+  {
+    mal_id: 39617,
+    url: 'https://myanimelist.net/anime/39617/Yakusoku_no_Neverland_2nd_Season',
+    title: 'Yakusoku no Neverland 2nd Season',
+    image_url: 'https://cdn.myanimelist.net/images/anime/1708/104922.jpg?s=4cdb0b349706bb17869c3e385cfd3978',
+    synopsis: 'Second season of Yakusoku no Neverland.',
+    type: 'TV',
+    airing_start: null,
+    episodes: null,
+    members: 102247,
+    genres: [{
+      mal_id: 24,
+      type: 'anime',
+      name: 'Sci-Fi',
+      url: 'https://myanimelist.net/anime/genre/24/Sci-Fi'
+    },
+    {
+      mal_id: 7,
+      type: 'anime',
+      name: 'Mystery',
+      url: 'https://myanimelist.net/anime/genre/7/Mystery'
+    },
+    {
+      mal_id: 40,
+      type: 'anime',
+      name: 'Psychological',
+      url: 'https://myanimelist.net/anime/genre/40/Psychological'
+    },
+    {
+      mal_id: 41,
+      type: 'anime',
+      name: 'Thriller',
+      url: 'https://myanimelist.net/anime/genre/41/Thriller'
+    },
+    {
+      mal_id: 27,
+      type: 'anime',
+      name: 'Shounen',
+      url: 'https://myanimelist.net/anime/genre/27/Shounen'
+    }
+    ],
+    source: 'Manga',
+    producers: [],
+    score: null,
+    licensors: [],
+    r18: false,
+    kids: false,
+    continuing: false
+  },
+  {
+    mal_id: 39551,
+    url: 'https://myanimelist.net/anime/39551/Tensei_shitara_Slime_Datta_Ken_2nd_Season',
+    title: 'Tensei shitara Slime Datta Ken 2nd Season',
+    image_url: 'https://cdn.myanimelist.net/images/anime/1153/99850.jpg?s=2dc82e6810ef7e25693d4a6669dca449',
+    synopsis: 'Second season of Tensei shitara Slime Datta Ken.',
+    type: 'TV',
+    airing_start: null,
+    episodes: null,
+    members: 101632,
+    genres: [{
+      mal_id: 4,
+      type: 'anime',
+      name: 'Comedy',
+      url: 'https://myanimelist.net/anime/genre/4/Comedy'
+    },
+    {
+      mal_id: 10,
+      type: 'anime',
+      name: 'Fantasy',
+      url: 'https://myanimelist.net/anime/genre/10/Fantasy'
+    }
+    ],
+    source: 'Light novel',
+    producers: [],
+    score: null,
+    licensors: [],
+    r18: false,
+    kids: false,
+    continuing: false
+  },
+  {
+    mal_id: 40356,
+    url: 'https://myanimelist.net/anime/40356/Tate_no_Yuusha_no_Nariagari_2nd_Season',
+    title: 'Tate no Yuusha no Nariagari 2nd Season',
+    image_url: 'https://cdn.myanimelist.net/images/anime/1008/102902.jpg?s=b4a2dd48f365b264dcf969d3b9bdcb3c',
+    synopsis: 'Second season of Tate no Yuusha no Nariagari.',
+    type: 'TV',
+    airing_start: null,
+    episodes: null,
+    members: 90092,
+    genres: [{
+      mal_id: 1,
+      type: 'anime',
+      name: 'Action',
+      url: 'https://myanimelist.net/anime/genre/1/Action'
+    },
+    {
+      mal_id: 2,
+      type: 'anime',
+      name: 'Adventure',
+      url: 'https://myanimelist.net/anime/genre/2/Adventure'
+    },
+    {
+      mal_id: 8,
+      type: 'anime',
+      name: 'Drama',
+      url: 'https://myanimelist.net/anime/genre/8/Drama'
+    },
+    {
+      mal_id: 10,
+      type: 'anime',
+      name: 'Fantasy',
+      url: 'https://myanimelist.net/anime/genre/10/Fantasy'
+    }
+    ],
+    source: 'Light novel',
+    producers: [],
+    score: null,
+    licensors: [],
+    r18: false,
+    kids: false,
+    continuing: false
+  },
+  {
+    mal_id: 40028,
+    url: 'https://myanimelist.net/anime/40028/Shingeki_no_Kyojin_The_Final_Season',
+    title: 'Shingeki no Kyojin The Final Season',
+    image_url: 'https://cdn.myanimelist.net/images/anime/1873/103180.jpg?s=946831f13050020a179995fc2239e11e',
+    synopsis: 'Final Season of Shingeki no Kyojin.',
+    type: 'TV',
+    airing_start: null,
+    episodes: null,
+    members: 74578,
+    genres: [{
+      mal_id: 1,
+      type: 'anime',
+      name: 'Action',
+      url: 'https://myanimelist.net/anime/genre/1/Action'
+    },
+    {
+      mal_id: 38,
+      type: 'anime',
+      name: 'Military',
+      url: 'https://myanimelist.net/anime/genre/38/Military'
+    },
+    {
+      mal_id: 7,
+      type: 'anime',
+      name: 'Mystery',
+      url: 'https://myanimelist.net/anime/genre/7/Mystery'
+    },
+    {
+      mal_id: 31,
+      type: 'anime',
+      name: 'Super Power',
+      url: 'https://myanimelist.net/anime/genre/31/Super_Power'
+    },
+    {
+      mal_id: 8,
+      type: 'anime',
+      name: 'Drama',
+      url: 'https://myanimelist.net/anime/genre/8/Drama'
+    },
+    {
+      mal_id: 10,
+      type: 'anime',
+      name: 'Fantasy',
+      url: 'https://myanimelist.net/anime/genre/10/Fantasy'
+    },
+    {
+      mal_id: 27,
+      type: 'anime',
+      name: 'Shounen',
+      url: 'https://myanimelist.net/anime/genre/27/Shounen'
+    }
+    ],
+    source: 'Manga',
+    producers: [],
+    score: null,
+    licensors: [],
+    r18: false,
+    kids: false,
+    continuing: false
+  },
+  {
+    mal_id: 40852,
+    url: 'https://myanimelist.net/anime/40852/Dr_Stone__Stone_Wars',
+    title: 'Dr. Stone: Stone Wars',
+    image_url: 'https://cdn.myanimelist.net/images/anime/1379/104911.jpg?s=ad3c22a3690f0ad047473bb84cb1dbd1',
+    synopsis: 'Second season of Dr. Stone.',
+    type: 'TV',
+    airing_start: null,
+    episodes: null,
+    members: 70326,
+    genres: [{
+      mal_id: 24,
+      type: 'anime',
+      name: 'Sci-Fi',
+      url: 'https://myanimelist.net/anime/genre/24/Sci-Fi'
+    },
+    {
+      mal_id: 2,
+      type: 'anime',
+      name: 'Adventure',
+      url: 'https://myanimelist.net/anime/genre/2/Adventure'
+    },
+    {
+      mal_id: 27,
+      type: 'anime',
+      name: 'Shounen',
+      url: 'https://myanimelist.net/anime/genre/27/Shounen'
+    }
+    ],
+    source: 'Manga',
+    producers: [],
+    score: null,
+    licensors: [],
+    r18: false,
+    kids: false,
+    continuing: false
+  },
+  {
+    mal_id: 39783,
+    url: 'https://myanimelist.net/anime/39783/5-toubun_no_Hanayome_2nd_Season',
+    title: '5-toubun no Hanayome 2nd Season',
+    image_url: 'https://cdn.myanimelist.net/images/anime/1322/100904.jpg?s=9744b32a264fe22bcee6cd8d4b297feb',
+    synopsis: 'Second season of 5-toubun no Hanayome. \r\n',
+    type: 'TV',
+    airing_start: null,
+    episodes: null,
+    members: 67096,
+    genres: [{
+      mal_id: 4,
+      type: 'anime',
+      name: 'Comedy',
+      url: 'https://myanimelist.net/anime/genre/4/Comedy'
+    },
+    {
+      mal_id: 35,
+      type: 'anime',
+      name: 'Harem',
+      url: 'https://myanimelist.net/anime/genre/35/Harem'
+    },
+    {
+      mal_id: 22,
+      type: 'anime',
+      name: 'Romance',
+      url: 'https://myanimelist.net/anime/genre/22/Romance'
+    },
+    {
+      mal_id: 23,
+      type: 'anime',
+      name: 'School',
+      url: 'https://myanimelist.net/anime/genre/23/School'
+    },
+    {
+      mal_id: 27,
+      type: 'anime',
+      name: 'Shounen',
+      url: 'https://myanimelist.net/anime/genre/27/Shounen'
+    }
+    ],
+    source: 'Manga',
+    producers: [],
+    score: null,
+    licensors: [],
+    r18: false,
+    kids: false,
+    continuing: false
+  },
+  {
+    mal_id: 39247,
+    url: 'https://myanimelist.net/anime/39247/Kobayashi-san_Chi_no_Maid_Dragon_2nd_Season',
+    title: 'Kobayashi-san Chi no Maid Dragon 2nd Season',
+    image_url: 'https://cdn.myanimelist.net/images/anime/1609/99019.jpg?s=8d3bd206faec56b2555af98088af97a5',
+    synopsis: 'Second season of Kobayashi-san Chi no Maid Dragon.',
+    type: 'TV',
+    airing_start: null,
+    episodes: null,
+    members: 51134,
+    genres: [{
+      mal_id: 36,
+      type: 'anime',
+      name: 'Slice of Life',
+      url: 'https://myanimelist.net/anime/genre/36/Slice_of_Life'
+    },
+    {
+      mal_id: 4,
+      type: 'anime',
+      name: 'Comedy',
+      url: 'https://myanimelist.net/anime/genre/4/Comedy'
+    },
+    {
+      mal_id: 10,
+      type: 'anime',
+      name: 'Fantasy',
+      url: 'https://myanimelist.net/anime/genre/10/Fantasy'
+    }
+    ],
+    source: 'Manga',
+    producers: [],
+    score: null,
+    licensors: [],
+    r18: false,
+    kids: false,
+    continuing: false
+  },
+  {
+    mal_id: 39586,
+    url: 'https://myanimelist.net/anime/39586/Hataraku_Saibou_2nd_Season',
+    title: 'Hataraku Saibou 2nd Season',
+    image_url: 'https://cdn.myanimelist.net/images/anime/1094/99970.jpg?s=17da76b1e0f87058f1d2247bb062099c',
+    synopsis: 'Second season of Hataraku Saibou.',
+    type: 'TV',
+    airing_start: null,
+    episodes: null,
+    members: 43041,
+    genres: [{
+      mal_id: 4,
+      type: 'anime',
+      name: 'Comedy',
+      url: 'https://myanimelist.net/anime/genre/4/Comedy'
+    },
+    {
+      mal_id: 27,
+      type: 'anime',
+      name: 'Shounen',
+      url: 'https://myanimelist.net/anime/genre/27/Shounen'
+    }
+    ],
+    source: 'Manga',
+    producers: [],
+    score: null,
+    licensors: [],
+    r18: false,
+    kids: false,
+    continuing: false
+  },
+  {
+    mal_id: 40507,
+    url: 'https://myanimelist.net/anime/40507/Arifureta_Shokugyou_de_Sekai_Saikyou_2nd_Season',
+    title: 'Arifureta Shokugyou de Sekai Saikyou 2nd Season',
+    image_url: 'https://cdn.myanimelist.net/images/anime/1198/103431.jpg?s=7ac0e044f58dfe31595267f570c930f7',
+    synopsis: 'Second season of Arifureta Shokugyou de Sekai Saikyou.',
+    type: 'TV',
+    airing_start: null,
+    episodes: null,
+    members: 34257,
+    genres: [{
+      mal_id: 1,
+      type: 'anime',
+      name: 'Action',
+      url: 'https://myanimelist.net/anime/genre/1/Action'
+    },
+    {
+      mal_id: 2,
+      type: 'anime',
+      name: 'Adventure',
+      url: 'https://myanimelist.net/anime/genre/2/Adventure'
+    },
+    {
+      mal_id: 35,
+      type: 'anime',
+      name: 'Harem',
+      url: 'https://myanimelist.net/anime/genre/35/Harem'
+    },
+    {
+      mal_id: 10,
+      type: 'anime',
+      name: 'Fantasy',
+      url: 'https://myanimelist.net/anime/genre/10/Fantasy'
+    }
+    ],
+    source: 'Light novel',
+    producers: [],
+    score: null,
+    licensors: [],
+    r18: false,
+    kids: false,
+    continuing: false
+  },
+  {
+    mal_id: 38474,
+    url: 'https://myanimelist.net/anime/38474/Yuru_Camp\u25b3_2nd_Season',
+    title: 'Yuru Camp\u25b3 2nd Season',
+    image_url: 'https://cdn.myanimelist.net/images/anime/1968/95339.jpg?s=611052cf0d60521f0dfa07f009626ad5',
+    synopsis: 'Second season of Yuru Camp\u25b3.',
+    type: 'TV',
+    airing_start: null,
+    episodes: null,
+    members: 34059,
+    genres: [{
+      mal_id: 4,
+      type: 'anime',
+      name: 'Comedy',
+      url: 'https://myanimelist.net/anime/genre/4/Comedy'
+    },
+    {
+      mal_id: 36,
+      type: 'anime',
+      name: 'Slice of Life',
+      url: 'https://myanimelist.net/anime/genre/36/Slice_of_Life'
+    }
+    ],
+    source: 'Manga',
+    producers: [],
+    score: null,
+    licensors: [],
+    r18: false,
+    kids: false,
+    continuing: false
+  },
+  {
+    mal_id: 40174,
+    url: 'https://myanimelist.net/anime/40174/Zombieland_Saga__Revenge',
+    title: 'Zombieland Saga: Revenge',
+    image_url: 'https://cdn.myanimelist.net/images/anime/1306/102300.jpg?s=168afbc9f92108a62addf23b7c2a79ee',
+    synopsis: 'Second Season of Zombieland Saga.',
+    type: 'TV',
+    airing_start: null,
+    episodes: null,
+    members: 33798,
+    genres: [{
+      mal_id: 19,
+      type: 'anime',
+      name: 'Music',
+      url: 'https://myanimelist.net/anime/genre/19/Music'
+    },
+    {
+      mal_id: 4,
+      type: 'anime',
+      name: 'Comedy',
+      url: 'https://myanimelist.net/anime/genre/4/Comedy'
+    },
+    {
+      mal_id: 37,
+      type: 'anime',
+      name: 'Supernatural',
+      url: 'https://myanimelist.net/anime/genre/37/Supernatural'
+    }
+    ],
+    source: 'Original',
+    producers: [],
+    score: null,
+    licensors: [],
+    r18: false,
+    kids: false,
+    continuing: false
+  },
+  {
+    mal_id: 40357,
+    url: 'https://myanimelist.net/anime/40357/Tate_no_Yuusha_no_Nariagari_3rd_Season',
+    title: 'Tate no Yuusha no Nariagari 3rd Season',
+    image_url: 'https://cdn.myanimelist.net/images/anime/1406/104631.jpg?s=1f3395ee723079a80b40d94f32e30199',
+    synopsis: 'Third season of Tate no Yuusha no Nariagari.',
+    type: 'TV',
+    airing_start: null,
+    episodes: null,
+    members: 30337,
+    genres: [{
+      mal_id: 1,
+      type: 'anime',
+      name: 'Action',
+      url: 'https://myanimelist.net/anime/genre/1/Action'
+    },
+    {
+      mal_id: 2,
+      type: 'anime',
+      name: 'Adventure',
+      url: 'https://myanimelist.net/anime/genre/2/Adventure'
+    },
+    {
+      mal_id: 8,
+      type: 'anime',
+      name: 'Drama',
+      url: 'https://myanimelist.net/anime/genre/8/Drama'
+    },
+    {
+      mal_id: 10,
+      type: 'anime',
+      name: 'Fantasy',
+      url: 'https://myanimelist.net/anime/genre/10/Fantasy'
+    }
+    ],
+    source: 'Light novel',
+    producers: [],
+    score: null,
+    licensors: [],
+    r18: false,
+    kids: false,
+    continuing: false
+  },
+  {
+    mal_id: 40729,
+    url: 'https://myanimelist.net/anime/40729/Megalo_Box_2',
+    title: 'Megalo Box 2',
+    image_url: 'https://cdn.myanimelist.net/images/anime/1942/104386.jpg?s=a29748f82012f2d2877183a221238995',
+    synopsis: 'Second season of Megalo Box.',
+    type: 'TV',
+    airing_start: null,
+    episodes: null,
+    members: 23395,
+    genres: [{
+      mal_id: 1,
+      type: 'anime',
+      name: 'Action',
+      url: 'https://myanimelist.net/anime/genre/1/Action'
+    },
+    {
+      mal_id: 24,
+      type: 'anime',
+      name: 'Sci-Fi',
+      url: 'https://myanimelist.net/anime/genre/24/Sci-Fi'
+    },
+    {
+      mal_id: 36,
+      type: 'anime',
+      name: 'Slice of Life',
+      url: 'https://myanimelist.net/anime/genre/36/Slice_of_Life'
+    },
+    {
+      mal_id: 30,
+      type: 'anime',
+      name: 'Sports',
+      url: 'https://myanimelist.net/anime/genre/30/Sports'
+    },
+    {
+      mal_id: 8,
+      type: 'anime',
+      name: 'Drama',
+      url: 'https://myanimelist.net/anime/genre/8/Drama'
+    }
+    ],
+    source: 'Original',
+    producers: [],
+    score: null,
+    licensors: [],
+    r18: false,
+    kids: false,
+    continuing: false
+  }
+]
+
+export default Animes
