e8fcad29fa741c64345aa94a5bee997b9027f112
Paulo Oliveira <paulofrauches@gmail.com>
2020-04-04 15:12:36 -0300

    test(app): Add navigation tests

--- src/App.test.js
+++ src/App.test.js
@@ -6,7 +6,7 @@ import App from './App';
 import Animes from './__mock__/Animes'
 
 describe('App', () => {
-  beforeAll(() => {
+  beforeEach(() => {
     nock('https://api.jikan.moe',{ allowUnmocked: false })
     .persist()
     .get('/v3/season/later')
@@ -35,19 +35,124 @@ describe('App', () => {
     expect(component.state('animes').length).toEqual(12)
   })
 
-  it('Should navigate down', () => {
-    const component = mount(<App />)
+  describe('Navigation', () => {
+    it('Shouldn`t navigate up when first item was focused', async () => {
+      const component = mount(<App />)
+      await component.instance().getData()
 
-    console.log(component.state())
+      document.dispatchEvent(new KeyboardEvent('keydown', { keyCode: 38 }))
+      component.update()
 
-    document.dispatchEvent(new KeyboardEvent('keydown', { keyCode: 39 }))
+      expect(component.state('activeItem')).toEqual(0)
+    })
 
-    console.log(component.state())
-    component.update()
-    console.log(component.state())
+    it('Should navigate up when focused item is in another line', async () => {
+      const component = mount(<App />)
+      await component.instance().getData()
+      component.setState({activeItem: 7})
+      component.update()
 
-    expect(component.state('activeItem')).toEqual(4)
+      document.dispatchEvent(new KeyboardEvent('keydown', { keyCode: 38 }))
+      component.update()
+
+      expect(component.state('activeItem')).toEqual(3)
+    })
+
+    it('Should navigate to first element when press up at first line', async () => {
+      const component = mount(<App />)
+      await component.instance().getData()
+      component.setState({activeItem: 2})
+      component.update()
+
+      document.dispatchEvent(new KeyboardEvent('keydown', { keyCode: 38 }))
+      component.update()
+
+      expect(component.state('activeItem')).toEqual(0)
+    })
+
+    it('Shouldn`t navigate down when last item was focused', async () => {
+      const component = mount(<App />)
+      await component.instance().getData()
+
+     component.setState({activeItem: 11})
+      component.update()
+
+      document.dispatchEvent(new KeyboardEvent('keydown', { keyCode: 40 }))
+      component.update()
+
+      expect(component.state('activeItem')).toEqual(11)
+    })
+
+    it('Should navigate down when focused item is in another line', async () => {
+      const component = mount(<App />)
+      await component.instance().getData()
+
+      document.dispatchEvent(new KeyboardEvent('keydown', { keyCode: 40 }))
+      component.update()
+
+      expect(component.state('activeItem')).toEqual(4)
+    })
+
+    it('Should navigate to last item when press down at last line', async () => {
+      const component = mount(<App />)
+      await component.instance().getData()
+      component.setState({activeItem: 10})
+      component.update()
+
+      document.dispatchEvent(new KeyboardEvent('keydown', { keyCode: 40 }))
+      component.update()
+
+      expect(component.state('activeItem')).toEqual(11)
+    })
+
+    it('Shouldn`t navigate right when last item was focused', async () => {
+      const component = mount(<App />)
+      await component.instance().getData()
+
+     component.setState({activeItem: 11})
+      component.update()
+
+      document.dispatchEvent(new KeyboardEvent('keydown', { keyCode: 39 }))
+      component.update()
+
+      expect(component.state('activeItem')).toEqual(11)
+    })
+
+    it('Should navigate right when focused not first item has focus', async () => {
+      const component = mount(<App />)
+      await component.instance().getData()
+
+      document.dispatchEvent(new KeyboardEvent('keydown', { keyCode: 39 }))
+      component.update()
+
+      expect(component.state('activeItem')).toEqual(1)
+    })
+
+    it('Shouldn`t navigate left when first item was focused', async () => {
+      const component = mount(<App />)
+      await component.instance().getData()
+
+
+      document.dispatchEvent(new KeyboardEvent('keydown', { keyCode: 37 }))
+      component.update()
+
+      expect(component.state('activeItem')).toEqual(0)
+    })
+
+    it('Should navigate left when not first item has focus', async () => {
+      const component = mount(<App />)
+      await component.instance().getData()
+      component.setState({activeItem: 8})
+      component.update()
+
+      document.dispatchEvent(new KeyboardEvent('keydown', { keyCode: 37 }))
+      component.update()
+
+      expect(component.state('activeItem')).toEqual(7)
+    })
+
   })
+
 
 })
 
